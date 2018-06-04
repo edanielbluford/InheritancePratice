5456562a9058ba78792a0e699cce6160087f86af
edanielbluford <e.daniel.bluford@gmail.com>
2018-06-04 11:46:42 -0400

    First Commit

---
+++ InheritancePractice/Species.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    public class Species
+    {
+        
+
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public string Habitat { get; set; }
+        public bool IsColdBlooded { get; set; }
+        public bool HasTail { get; set; }
+
+
+
+    }
+}
